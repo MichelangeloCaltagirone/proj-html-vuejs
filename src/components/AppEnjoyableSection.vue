9557301b2f5db663993ddc804c17a825816d3e30
JoseManuel-Feliz <jmgarcia1718@gmail.com>
2024-09-25 15:58:58 +0200

    added: layout (container, row, cols)

--- src/components/AppEnjoyableSection.vue
+++ src/components/AppEnjoyableSection.vue
@@ -9,7 +9,19 @@ export default {
 </script>
 
 <template>
+    <div class="conatiner">
+        <div class="row">
 
+            <div class="col left">
+                <h2>text for test</h2>
+            </div>
+            <div class="col center"></div>
+            <h2>text for test</h2>
+        </div>
+        <div class="col left">
+            <h2>text for test</h2>
+        </div>
+    </div>
 </template>
 
 <style lang="scss" scoped></style>
