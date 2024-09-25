e12daa6ee10455d64c7e55ddb77c1f4d499489df
JoseManuel-Feliz <jmgarcia1718@gmail.com>
2024-09-25 17:16:17 +0200

    added basic layout to Enjoyable insights section

--- src/components/AppEnjoyableSection.vue
+++ src/components/AppEnjoyableSection.vue
@@ -3,25 +3,104 @@ export default {
     data() {
         return {
 
+            insinghts: [
+                {
+                    link: " Become a Better Blogger: Content Planning",
+                    icon: "2190",
+                },
+
+                {
+                    link: "Promoting Your Online Business on Pinterest",
+                    icon: "2190",
+                },
+
+                {
+                    link: "Gamification and Game- Based Learning",
+                    icon: "2190",
+                },
+
+                {
+                    link: "Designing an Online Course from Experts Perspective",
+                    icon: "2190",
+                },
+
+                {
+                    link: "Why Online Courses Are the Future of Education",
+                    icon: "2190",
+                },
+
+
+            ],
         }
     },
+
 }
 </script>
 
 <template>
-    <div class="conatiner">
-        <div class="row">
+    <section>
 
-            <div class="col left">
-                <h2>text for test</h2>
+        <div class="conatiner">
+
+
+            <div class="row">
+
+                <div class="col">
+                    <span>Enjoyable insights</span>
+
+                    <h3>Most Viewed Best Blogs</h3>
+                    <ul>
+                        <li v-for="item in insinghts">{{ item.link }}</li>
+                    </ul>
+                </div>
+                <div class="col center card">
+
+                    <div class="card">
+                        <div class="fakeImg"></div>
+                        <div class="card-text"></div>
+                    </div>
+
+                </div>
             </div>
-            <div class="col center"></div>
-            <h2>text for test</h2>
+            <div class="col right">
+                <div class="card">
+                    <div class="fakeImg"></div>
+                    <div class="card-text"></div>
+                </div>
+
+            </div>
         </div>
-        <div class="col left">
-            <h2>text for test</h2>
-        </div>
-    </div>
+    </section>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+section {
+    background-color: #F5F1ED;
+
+    .container {
+        max-width: 1200px;
+        margin: 0 auto;
+
+        .row {
+            display: flex;
+            flex-direction: row;
+
+
+            .col {
+                flex-basis: calc(100% / 3);
+
+                ul {
+                    list-style-type: none;
+                }
+            }
+
+            .card {
+                height: 250px;
+                width: 200;
+
+            }
+
+        }
+    }
+}
+</style>
