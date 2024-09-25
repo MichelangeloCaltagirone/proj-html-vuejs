ba71d7ef140d0b39abfd5d68fc56f08254b00235
JoseManuel-Feliz <jmgarcia1718@gmail.com>
2024-09-25 22:25:50 +0200

    recovered the App.vue file

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,46 @@
 <script>
 import OnlineCourses from './components/AppOnlineCourses.vue';
+import Jumbotron from './components/Jumbotron.vue';
+import PageHeader from './components/PageHeader.vue';
+import EnjoyableSection from './components/AppEnjoyableSection.vue';
+
 export default {
   data() {
     return {
-
+      menuItems: [
+        { name: 'Home', link: '#' },
+        { name: 'Pages', link: '#' },
+        { name: 'Courses', link: '#' },
+        { name: 'Features', link: '#' },
+        { name: 'Blog', link: '#' },
+        { name: 'Shop', link: '#' },
+      ],
     }
   },
   components: {
-    OnlineCourses
-  }
-}
+    OnlineCourses,
+    PageHeader,
+    EnjoyableSection,
+    Jumbotron,
 
+  },
+}
 </script>
 
 <template>
+
   <div>
     <PageHeader :menuItems="menuItems" />
     <Jumbotron title="Distant learning for further expansion"
       subtitle="Learning is a life-long journey that in fact we never find the terminate stop. Stop searching, enjoy the process."
       buttonText="Download free guide" />
+
   </div>
   <OnlineCourses />
+  <EnjoyableSection />
 
 </template>
+
 
 <style lang="scss">
 @use './style/general.scss' as *;
--- src/components/OnlineCoursesCard.vue
+++ src/components/OnlineCoursesCard.vue
@@ -1,58 +1,57 @@
 <script>
-    export default {
-        data() {
-            return {
-
-            }
-        },
-        props: {
-            picPath: {
-                type: String,
-                required: true
-            },
-            price: {
-                type: String,
-                required: true
-            },
-            name: {
-                type: String,
-                required: true
-            },
-            students: {
-                type: Number,
-                required: true
-            },
-            lessons: {
-                type: Number,
-                required: true
-            }
+export default {
+    data() {
+        return {
 
         }
+    },
+    props: {
+        picPath: {
+            type: String,
+            required: true
+        },
+        price: {
+            type: String,
+            required: true
+        },
+        name: {
+            type: String,
+            required: true
+        },
+        students: {
+            type: Number,
+            required: true
+        },
+        lessons: {
+            type: Number,
+            required: true
+        }
+
     }
+}
 </script>
 
 <template>
 
-<div class="container">
-    <div class="card mb-3" style="max-width: 540px;">
-        <div class="row g-0">
-            <div class="col-md-4">
-                <img :src="picPath" class="img-fluid rounded-start" :alt='picPath'>
-            </div>
-            <div class="col-md-8">
-                <div class="card-body">
-                    <h5 class="card-title">{{ price }}</h5>
-                    <p class="card-text"> {{ name }}</p>
-                    <p class="card-text"><small class="text-body-secondary">i {{ lessons }} lessons</small></p>
-                    <p class="card-text"><small class="text-body-secondary">i {{ students }} students</small></p>
+    <div class="container">
+        <div class="card mb-3" style="max-width: 540px;">
+            <div class="row g-0">
+                <div class="col-md-4">
+                    <img :src="picPath" class="img-fluid rounded-start" :alt='picPath'>
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h5 class="card-title">{{ price }}</h5>
+                        <p class="card-text"> {{ name }}</p>
+                        <p class="card-text"><small class="text-body-secondary">i {{ lessons }} lessons</small></p>
+                        <p class="card-text"><small class="text-body-secondary">i {{ students }} students</small></p>
 
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>    
 
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
