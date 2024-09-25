872e9bba4c9bfd62692cf782f2054e53a5d1e68f
MichelangeloCaltagirone <michelangelocaltagirone@gmail.com>
2024-09-25 17:02:28 +0200

    Add and beging to style the AppOnlineCourses.vue, with his component and data

--- src/components/OnlineCoursesCard.vue
+++ src/components/OnlineCoursesCard.vue
@@ -19,11 +19,11 @@
                 required: true
             },
             students: {
-                type: number,
+                type: Number,
                 required: true
             },
             lessons: {
-                type: number,
+                type: Number,
                 required: true
             }
 
@@ -37,13 +37,15 @@
     <div class="card mb-3" style="max-width: 540px;">
         <div class="row g-0">
             <div class="col-md-4">
-                <img src="..." class="img-fluid rounded-start" alt="...">
+                <img :src="picPath" class="img-fluid rounded-start" :alt='picPath'>
             </div>
             <div class="col-md-8">
                 <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                    <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+                    <h5 class="card-title">{{ price }}</h5>
+                    <p class="card-text"> {{ name }}</p>
+                    <p class="card-text"><small class="text-body-secondary">i {{ lessons }} lessons</small></p>
+                    <p class="card-text"><small class="text-body-secondary">i {{ students }} students</small></p>
+
                 </div>
             </div>
         </div>
