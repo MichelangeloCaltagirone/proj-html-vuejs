67eb9dc06ea46d15a417dcbe471479cbec72637f
Andrea Gulli <andreagulli11@gmail.com>
2024-09-26 09:47:02 +0200

    First push

--- src/main.js
+++ src/main.js
@@ -1,9 +1,8 @@
-import { createApp } from 'vue'
 import '@fortawesome/fontawesome-free/css/all.min.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { createApp } from 'vue';
 
-
-import App from './App.vue'
+import App from './App.vue';
 
 createApp(App).mount('#app')
