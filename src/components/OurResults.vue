57af380eaa2e757bac5511733e1e9d91c2c27830
JoseManuel-Feliz <jmgarcia1718@gmail.com>
2024-09-26 16:18:27 +0200

    Our Results section (create OurResults component, added OurResults array of objects

---
+++ src/components/OurResults.vue
@@ -0,0 +1,39 @@
+<script>
+export default {
+    data() {
+        return {
+            nonloso:
+                [
+
+                    {
+                        name: 'Successfully trained',
+                        num: 1790,
+                        cose: 'Enrolled Learners'
+                    },
+                    {
+                        name: 'Proudly Received',
+                        num: 19,
+                        cose: 'Countrywide Awards'
+                    },
+                    {
+                        name: 'Firmly Established',
+                        num: 24,
+                        cose: 'local branches'
+                    },
+                    {
+                        name: 'Getting Featured on',
+                        num: 1090,
+                        cose: 'Blog Posts'
+                    },
+                ]
+
+        }
+    }
+}
+</script>
+
+<template>
+
+</template>
+
+<style lang="scss" scoped></style>
