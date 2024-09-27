2e6e390b31c02b82c319225e1db7d150a5614aef
Jonathan-Ultini <jonathanultini@gmail.com>
2024-09-27 11:43:09 +0200

    fix order jumbo mobile view

--- src/components/Jumbotron.vue
+++ src/components/Jumbotron.vue
@@ -2,7 +2,7 @@
   <div class="jumbotron text-dark p-5 mt-5">
     <div class="container">
       <div class="row align-items-center">
-        <div class="col-md-6">
+        <div class="col-md-6 order-2 order-md-1">
           <h1 class="display-4">{{ title }}</h1>
           <p class="lead">{{ subtitle }}</p>
           <a href="#" class="btn btn-success">
@@ -20,7 +20,7 @@
 
         </div>
 
-        <div class="col-md-6">
+        <div class="col-md-6 order-1 order-md-2">
           <img src="../assets/images/about-me-profile-ohm4dxoearqbxny7q3bq1pzbdgofdx0tobbeqcvzd0.jpg"
             alt="Jumbotron Image" class="img-fluid rounded-circle">
         </div>
