9ed098e1031beb7a4ed353fae2c9618c8e19ba2e
Andrea Gulli <andreagulli11@gmail.com>
2024-09-27 17:13:38 +0200

    fix main import error

--- src/main.js
+++ src/main.js
@@ -1,10 +1,9 @@
 import '@fortawesome/fontawesome-free/css/all.min.css';
+import '@qpokychuk/gilroy/index.css';
+import '@qpokychuk/gilroy/italic.css';
+import '@qpokychuk/gilroy/normal.css';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'animate.css';
-import '@qpokychuk/gilroy/index.css';
-import '@qpokychuk/gilroy/normal.css';
-import '@qpokychuk/gilroy/italic.css';
 import { createApp } from 'vue';
 
 import App from './App.vue';
