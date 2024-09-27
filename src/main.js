a840a5060ae92962b4093d9a5bc6abe98fd303b1
Andrea Gulli <andreagulli11@gmail.com>
2024-09-27 13:17:17 +0200

    fix import error

--- src/main.js
+++ src/main.js
@@ -2,10 +2,6 @@ import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-
-import '@qpokychuk/gilroy/italic.css';
-import '@qpokychuk/gilroy/index.css';
-import '@qpokychuk/gilroy/normal.css';
 import { createApp } from 'vue';
 
 import App from './App.vue';
