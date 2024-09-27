67b3858f2a0aa668e201fe2391dd102435adee9a
Jonathan-Ultini <jonathanultini@gmail.com>
2024-09-27 16:34:55 +0200

    fix subscribe

--- src/components/subscribeNewsletterSection.vue
+++ src/components/subscribeNewsletterSection.vue
@@ -9,32 +9,32 @@ export default {
 </script>
 
 <template>
-    <section class="py-5 my-5 w-75 m-auto">
+    <section class="py-5 my-5 m-auto text-center">
 
         <div class="container">
 
-            <div class="row row-cols-1 align-items-center d-flex justify-content-center align-items-center">
+            <div class="row justify-content-center align-items-center">
 
-                <div class="col">
-
-                    <div class="fw-bold fs-1 my-5">Subscribe <span class="c-green fw-medium">Newsletters</span></div>
-                    <p class="c-grey fs-3 text-wrap">Enter your email address to register to our newsletter subscription
-                        delivered on a regular basis!</p>
+                <div class="col-lg-8 mb-4">
+                    <h2 class="fw-bold fs-1 my-4">Subscribe to <span class="c-green fw-medium">Newsletters</span></h2>
+                    <p class="c-grey fs-5">Enter your email address to register for our newsletter, delivered on a
+                        regular basis!</p>
                 </div>
 
-                <div class="col">
-
-                    <div class="input-group input-group-lg mb-3 ">
-                        <div class="form-floating">
-                            <input type="email" class="form-control bg-grey border border-0" id="SubscribeNewsletter"
+                <div class="col-lg-5">
+                    <div class="input-group input-group-lg mb-3">
+                        <div class="form-floating flex-grow-1">
+                            <input type="email" class="form-control bg-grey border-0" id="SubscribeNewsletter"
                                 placeholder="Enter your email">
                             <label for="SubscribeNewsletter">Enter your email</label>
                         </div>
-                        <button class="btn btn-lg input-group-text bg-green">Confirm identity</button>
+                        <button class="btn btn-lg input-group-text bg-green text-white">Subscribe</button>
                     </div>
                 </div>
             </div>
+
         </div>
+
     </section>
 </template>
 
